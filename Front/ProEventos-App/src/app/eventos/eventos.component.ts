9f1491e2093308b4d926e0f4e2a5f737b07e89c6
vinicius-ssantos <viniciusolicveira@gmail.com>
2022-04-07 17:27:57 -0300

    commit 07-04-2022

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,20 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
-  public eventos: any;
+  public eventos: any = [];
+  widthImg: number = 150;
+  marginImg: number = 2;
+  exibirImagem:boolean=true;
 
-  constructor(private http:HttpClient) {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.getEventos();
   }
+
+alterarImagem(){
+  this.exibirImagem= !this.exibirImagem;
+}
+
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
-      error => console.log(error)
+      (response) => (this.eventos = response),
+      (error) => console.log(error)
     );
-
-
-    
   }
 }
