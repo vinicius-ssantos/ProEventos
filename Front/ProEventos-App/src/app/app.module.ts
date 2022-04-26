5067f9333d65b8bdd4977a270b9f6ffe26f107cc
vinicius-ssantos <viniciusolicveira@gmail.com>
2022-04-26 16:35:52 -0300

    Atualizando para angular 11

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,9 +1,16 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from  '@angular/common/http'
 import { FormsModule } from '@angular/forms';
 
+
+import {CollapseModule} from 'ngx-bootstrap/collapse'
+import{TooltipModule} from'ngx-bootstrap/tooltip';
+import{BsDropdownModule} from'ngx-bootstrap/dropdown';
+import{ModalModule} from 'ngx-bootstrap/modal';
+import{ToastrModule} from 'ngx-toastr'
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,25 +18,40 @@ import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { NavComponent } from './nav/nav.component';
 
-import {CollapseModule} from 'ngx-bootstrap/collapse'
+import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
+
+import { EventoService } from './services/Evento.service';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
       PalestrantesComponent,
-      NavComponent
+      NavComponent,
+      DateTimeFormatPipe
    ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
     CollapseModule.forRoot(),
-    FormsModule
-
+    TooltipModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    ModalModule.forRoot(),
+    ToastrModule.forRoot({
+      timeOut: 4000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+      progressBar:true
+    }),
+    NgxSpinnerModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [EventoService],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
